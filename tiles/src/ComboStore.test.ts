9152f115074cd1a05dd499e2104118a5df98da09
Donnie Ahmad <donnie.ahmad@appfolio.com>
2024-01-02 09:28:57 -0800

    Add tile matching logic to ComboStore

--- tiles/src/ComboStore.test.ts
+++ tiles/src/ComboStore.test.ts
@@ -81,3 +81,78 @@ it('can reset the selected tile', () => {
     comboStore.resetSelectedTileIndex();
     expect(comboStore.selectedTileIndex).toBeUndefined();
 })
+
+it('increments current combo count if tiles match', () => {
+    const firstTile = { contents: [1,2,4] }
+    const secondTile = { contents: [4,5,6] }
+
+    const comboStore = new ComboStore([firstTile, secondTile]);
+    comboStore.setSelectedTileIndex(0);
+    comboStore.matchTiles(1);
+
+    expect(comboStore.currentComboCounter).toEqual(1);
+    expect(comboStore.selectedTileIndex).toBeUndefined();
+})
+
+it('resets current combo count if tiles don\'t match', () => {
+    const firstTile = { contents: [1,2,3] }
+    const secondTile = { contents: [4,5,6] }
+
+    const comboStore = new ComboStore([firstTile, secondTile]);
+    comboStore.currentComboCounter = 100;
+    comboStore.setSelectedTileIndex(0);
+    comboStore.matchTiles(1);
+
+    expect(comboStore.currentComboCounter).toEqual(0);
+    expect(comboStore.selectedTileIndex).toBeUndefined();
+})
+
+it('modifies tiles if tiles match -- single match', () => {
+    const firstTile = { contents: [1,2,4] }
+    const secondTile = { contents: [4,5,6] }
+
+    const comboStore = new ComboStore([firstTile, secondTile]);
+    comboStore.setSelectedTileIndex(0);
+    comboStore.matchTiles(1);
+
+    expect(comboStore.tiles[0]).toEqual({ contents: [1,2] });
+    expect(comboStore.tiles[1]).toEqual({ contents: [5,6] });
+})
+
+it('modifies tiles if tiles match -- many matches', () => {
+    const firstTile = { contents: [1,2,3,4,5,6,7,8] }
+    const secondTile = { contents: [4,5,6,7,8,9] }
+
+    const comboStore = new ComboStore([firstTile, secondTile]);
+    comboStore.setSelectedTileIndex(0);
+    comboStore.matchTiles(1);
+
+    expect(comboStore.tiles[0]).toEqual({ contents: [1,2,3] });
+    expect(comboStore.tiles[1]).toEqual({ contents: [9] });
+})
+
+it('modifies tiles if tiles match -- final match', () => {
+    const firstTile = { contents: [1] }
+    const secondTile = { contents: [1] }
+
+    const comboStore = new ComboStore([firstTile, secondTile]);
+    comboStore.setSelectedTileIndex(0);
+    comboStore.matchTiles(1);
+
+    expect(comboStore.tiles[0]).toEqual({ contents: [] });
+    expect(comboStore.tiles[1]).toEqual({ contents: [] });
+})
+
+it('does not modify tiles if tiles don\'t match', () => {
+    const firstTile = { contents: [1,2,3] }
+    const secondTile = { contents: [4,5,6] }
+
+    const comboStore = new ComboStore([firstTile, secondTile]);
+    comboStore.currentComboCounter = 100;
+    comboStore.setSelectedTileIndex(0);
+    comboStore.matchTiles(1);
+
+    expect(comboStore.tiles[0]).toEqual(firstTile);
+    expect(comboStore.tiles[1]).toEqual(secondTile);
+})
+
--- tiles/src/ComboStore.ts
+++ tiles/src/ComboStore.ts
@@ -18,6 +18,27 @@ export default class ComboStore {
         this.currentComboCounter += 1;
     }
 
+    @action matchTiles = (index: number): void => {
+        if (this.selectedTileIndex !== undefined) {
+            const firstTile = this.tiles[this.selectedTileIndex];
+            const secondTile = this.tiles[index];
+
+            const contentsIntersection = firstTile.contents.filter(value => secondTile.contents.includes(value));
+
+            if (contentsIntersection.length > 0) {
+                this.incrementCurrentComboCounter();
+
+                firstTile.contents = firstTile.contents.filter(value => !contentsIntersection.includes(value))
+                secondTile.contents = secondTile.contents.filter(value => !contentsIntersection.includes(value))
+
+                this.resetSelectedTileIndex();
+            } else {
+                this.resetCurrentComboCounter();
+                this.resetSelectedTileIndex();
+            }
+        }
+    }
+
     @action resetCurrentComboCounter = (): void => {
         this.comboCounts.push(this.currentComboCounter);
         this.currentComboCounter = 0;
