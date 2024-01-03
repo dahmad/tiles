55ec07420a822a28eb49b34abf5739883e4dedf8
Donnie Ahmad <donnie.ahmad@appfolio.com>
2024-01-02 16:19:45 -0800

    Add Tile component

---
+++ tiles/src/components/Tile.tsx
@@ -0,0 +1,40 @@
+import { observer } from 'mobx-react-lite';
+import { FC, useContext } from 'react';
+import ComboStore from '../ComboStore';
+import { RootContext } from '../RootContext';
+import './Tile.css';
+
+interface TileProps {
+    rowIndex: number;
+    columnIndex: number;
+}
+
+const shouldSetSelectedTileIndex = (comboStore: ComboStore, rowIndex: number, columnIndex: number): boolean => {
+    if (comboStore.selectedTileIndex === undefined) {
+        return true;
+    } else {
+        return comboStore.selectedTileIndex[0] === rowIndex && comboStore.selectedTileIndex[1] === columnIndex;
+    }
+}
+
+const Tile: FC<TileProps> = ({ rowIndex, columnIndex }) => {
+    const { comboStore } = useContext(RootContext);
+
+    return (
+        <button
+            id={`tile_${rowIndex}_${columnIndex}`}
+            disabled={comboStore.tileSet[rowIndex][columnIndex].contents.length === 0}
+            onClick={() => {
+                if (shouldSetSelectedTileIndex(comboStore, rowIndex, columnIndex)) {
+                    comboStore.setSelectedTileIndex(rowIndex, columnIndex)
+                } else {
+                    comboStore.matchTiles(rowIndex, columnIndex)
+                }
+            }}
+        >
+            {String(comboStore.tileSet[rowIndex][columnIndex].contents)}
+        </button>
+    );
+};
+
+export default observer(Tile);
