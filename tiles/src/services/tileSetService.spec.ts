af81e5eca4ee6130abab9c7394613d3ddd0fddec
Donnie Ahmad <donnie.ahmad@appfolio.com>
2024-01-04 23:24:49 -0800

    Add tileSetService

---
+++ tiles/src/services/tileSetService.spec.ts
@@ -0,0 +1,148 @@
+import * as sinon from 'sinon';
+import { ComponentData } from '../types/ComponentData';
+import { TileData } from '../types/TileData';
+import TileSetService from './tileSetService';
+
+const mockComponentData = (componentTypeName: string): ComponentData => {
+  return {
+    componentTypeName,
+    id: '',
+    svg: '',
+  };
+};
+
+it('takes theme path and dimenions in constructor', () => {
+  const tileSetService = new TileSetService('path/to/foo', 2, 2);
+  expect(tileSetService.themePath).toEqual('path/to/foo');
+  expect(tileSetService.rowSize).toEqual(2);
+  expect(tileSetService.columnSize).toEqual(2);
+});
+
+it('throws an error if provided odd number of tiles', () => {
+  const t = () => {
+    new TileSetService('', 3, 3);
+  };
+  expect(t).toThrow(Error);
+});
+
+describe('findIndexesOfTilesMissingComponentType()', () => {
+  const tileSetService = new TileSetService('path/to/foo', 2, 2);
+  const tileSet = [
+    [
+      mockComponentData('foo'),
+      mockComponentData('bar'),
+      mockComponentData('baz'),
+    ],
+    [mockComponentData('foo')],
+    [mockComponentData('foo')],
+  ];
+
+  it('has tiles missing type', () => {
+    const results = tileSetService.findIndexesOfTilesMissingComponentType(
+      tileSet,
+      'bar'
+    );
+
+    expect(results).toEqual([1, 2]);
+  });
+
+  it('does not have tiles missing type', () => {
+    const results = tileSetService.findIndexesOfTilesMissingComponentType(
+      tileSet,
+      'foo'
+    );
+
+    expect(results).toEqual([]);
+  });
+});
+
+describe('formatTileSetData()', () => {
+  it('formats tile set data', () => {
+    const tileSetService = new TileSetService('path/to/foo', 2, 2);
+    const input = [[], [], [], []];
+    const expected = [
+      [[], []],
+      [[], []],
+    ];
+    expect(tileSetService.formatTileSetData(input)).toEqual(expected);
+  });
+});
+
+describe('generateTileSetData()', () => {
+  it('generates tile set data', () => {
+    let tileSetService = new TileSetService('path/to/foo', 2, 2);
+    const readThemeStub = sinon.stub();
+    readThemeStub.returns({
+      name: 'Foo Theme',
+      componentTypes: [
+        {
+          name: 'back',
+          components: [
+            { id: '1', svg: '1' },
+            { id: '2', svg: '2' },
+          ],
+        },
+        {
+          name: 'front',
+          components: [
+            { id: '3', svg: '3' },
+            { id: '4', svg: '4' },
+          ],
+        },
+      ],
+    });
+    tileSetService.readTheme = readThemeStub;
+
+    tileSetService.generateTileSetData();
+
+    expect(tileSetService.tileSet.length).toEqual(4);
+    tileSetService.tileSet.every((tile: TileData) => {
+      expect(tile.length).toEqual(2);
+    });
+  });
+});
+
+describe('getEmptyTilesArray()', () => {
+  it('generates empty tiles array', () => {
+    let tileSetService = new TileSetService('path/to/foo', 2, 2);
+    let expected = [[], [], [], []];
+    expect(tileSetService.getEmptyTilesArray()).toEqual(expected);
+
+    tileSetService = new TileSetService('path/to/foo', 2, 3);
+    expected = [[], [], [], [], [], []];
+    expect(tileSetService.getEmptyTilesArray()).toEqual(expected);
+  });
+});
+
+describe('pushComponent()', () => {
+  it('adds component to tileset', () => {
+    let tileSetService = new TileSetService('path/to/foo', 2, 2);
+    expect(tileSetService.tileSet).toEqual([[], [], [], []]);
+    tileSetService.pushComponent('type1', { id: 'id1', svg: 'svg1' });
+    expect(tileSetService.tileSet).toEqual([
+      [],
+      [],
+      [],
+      [{ componentTypeName: 'type1', id: 'id1', svg: 'svg1' }],
+    ]);
+
+    tileSetService.pushComponent('type1', { id: 'id2', svg: 'svg2' });
+    expect(tileSetService.tileSet).toEqual([
+      [],
+      [],
+      [{ componentTypeName: 'type1', id: 'id1', svg: 'svg1' }],
+      [{ componentTypeName: 'type1', id: 'id2', svg: 'svg2' }],
+    ]);
+  });
+});
+
+describe('readTheme()', () => {
+  it('loads JSON from path', () => {
+    let tileSetService = new TileSetService(
+      'src/assets/themes/hongKong.json',
+      2,
+      2
+    );
+    expect(tileSetService.readTheme()).toBeDefined();
+  });
+});
---
+++ tiles/src/services/tileSetService.ts
@@ -0,0 +1,107 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { Theme } from '../types/Theme';
+import { ThemeComponent } from '../types/ThemeComponent';
+import { ThemeComponentType } from '../types/ThemeComponentType';
+import { TileData } from '../types/TileData';
+import { TileSetData } from '../types/TileSetData';
+
+export default class TileSetService {
+  themePath: string;
+  rowSize: number;
+  columnSize: number;
+  tileSet: TileData[];
+
+  constructor(themePath: string, rowSize: number, columnSize: number) {
+    if ((rowSize * columnSize) % 2 !== 0) {
+      throw new Error('Number of tiles must be an even number');
+    }
+
+    this.themePath = themePath;
+    this.rowSize = rowSize;
+    this.columnSize = columnSize;
+    this.tileSet = this.getEmptyTilesArray();
+  }
+
+  findIndexesOfTilesMissingComponentType = (
+    tileSet: TileData[],
+    componentTypeName: string
+  ): number[] => {
+    let indexes: number[] = [];
+
+    tileSet.forEach((tile: TileData, i: number) => {
+      let hasComponentType = tile.find(
+        (component: any) => component.componentTypeName === componentTypeName
+      );
+      if (!hasComponentType) {
+        indexes.push(i);
+      }
+    });
+
+    return indexes;
+  };
+
+  formatTileSetData = (tileSet: TileData[]): TileSetData => {
+    let output: TileSetData = [];
+
+    for (let i = 0; i < tileSet.length; i += this.rowSize) {
+      output.push(tileSet.slice(i, i + this.rowSize));
+    }
+
+    return output;
+  };
+
+  generateTileSetData = (): void => {
+    const theme = this.readTheme();
+
+    let componentTypeName: string;
+
+    theme.componentTypes.forEach((componentType: ThemeComponentType) => {
+      componentTypeName = componentType.name;
+      for (let i = 0; i < (this.rowSize * this.columnSize) / 2; i++) {
+        let randomComponent =
+          componentType.components[
+            ~~(Math.random() * componentType.components.length)
+          ];
+        this.pushComponent(componentTypeName, randomComponent);
+        this.pushComponent(componentTypeName, randomComponent);
+      }
+    });
+  };
+
+  getEmptyTilesArray = (): TileData[] => {
+    return Array(this.rowSize * this.columnSize).fill([]);
+  };
+
+  pushComponent = (
+    componentTypeName: string,
+    randomComponent: ThemeComponent
+  ): void => {
+    let indexes = this.findIndexesOfTilesMissingComponentType(
+      this.tileSet,
+      componentTypeName
+    );
+    let index = indexes[~~(Math.random() * indexes.length)];
+    let tileCopy = new Array(...this.tileSet[index]);
+    tileCopy.push({
+      componentTypeName: componentTypeName,
+      id: randomComponent.id,
+      svg: randomComponent.svg,
+    });
+
+    let tileSetCopy = this.tileSet
+      .slice(0, index)
+      .concat(this.tileSet.slice(index + 1));
+    tileSetCopy.push(tileCopy);
+
+    this.tileSet = tileSetCopy;
+  };
+
+  readTheme = (): Theme => {
+    const buffer = readFileSync(this.themePath);
+    return JSON.parse(buffer as unknown as string);
+  };
+
+  writeTileSetData = (tileSetData: TileSetData, path: string): void => {
+    writeFileSync(path, JSON.stringify(tileSetData, null, 2));
+  };
+}
