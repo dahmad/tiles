ecf941bace699dd02fef797e263f503306353a33
Donnie Ahmad <donnie.ahmad@appfolio.com>
2024-01-08 17:51:02 -0800

    Call tile set service from FastAPI

--- app/main.py
+++ app/main.py
@@ -1,8 +1,48 @@
-from fastapi import FastAPI
+from json import load
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from .tile_sets import THEMES, generate_tile_set, load_theme
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",  # React app
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+TEST_TILESET_FILE_PATH = f"./src/assets/tileSets/testTileSet.json"
+
+
+@app.get("/themes")
+def read_themes() -> List[str]:
+    return THEMES
+
+
+@app.get("/theme/{theme}")
+def read_theme(theme: str) -> dict:
+    return load_theme(theme)
+
+
+@app.get("/theme/{theme}/generate")
+def read_generated_tile_set(theme: str, rowSize: int = 5, columnSize: int = 6) -> List:
+    if (rowSize * columnSize) % 2 != 0:
+        raise HTTPException(
+            status_code=400, detail="Number of tiles must be an even number"
+        )
+
+    if theme == "test":
+        with open(TEST_TILESET_FILE_PATH, "r", encoding="utf-8") as handler:
+            return load(handler)
+
+    theme = load_theme(theme)
+    return generate_tile_set(theme, rowSize, columnSize)
